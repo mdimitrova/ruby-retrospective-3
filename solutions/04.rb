dd8e5117b4613a1202a583649e4e9c7a41c2ec14
Monica <monicabdimitrova@gmail.com>
2014-01-22 17:00:10 +0200

    Remove test from 04.rb

--- solutions/04.rb
+++ solutions/04.rb
@@ -86,18 +86,3 @@ module Asm
     end
   end
 end
-
-Asm.asm do
-  mov ax, 40
-  mov bx, 32
-  label cycle
-  cmp ax, bx
-  je finish
-  jl asmaller
-  dec ax, bx
-  jmp cycle
-  label asmaller
-  dec bx, ax
-  jmp cycle
-  label finish
-end                 # => [8, 8, 0, 0]
